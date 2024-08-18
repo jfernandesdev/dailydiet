63d65f2b3983330822a49694c86867ae7331728e
Jeferson Fernandes <jfernandes.dev@gmail.com>
2024-08-18 20:50:24 -0300

    FIX: fix ts setting theme

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -3,6 +3,6 @@ import { THEME } from '../theme';
 
 type ThemeType = typeof THEME;
 
-declare module 'styled-components' {
+declare module 'styled-components/native' {
   export interface DefaultTheme extends ThemeType {}
 }
